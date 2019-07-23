64a9f3fc363408d45a912a4606a9aa53569fd1ba
[swishyfishie] <[pclandrei@gmail.com]>
2019-07-23 19:48:47 +0100

    added mobile dropdown

--- js/script.js
+++ js/script.js
@@ -1,43 +1,33 @@
-const psyBtn = document.querySelector('.dropdown-psy');
-const mhBtn = document.querySelector('.dropdown-mh');
-const persBtn = document.querySelector('.dropdown-pers');
+
 const contentPsy = document.querySelector('.content-psy');
 const contentMh = document.querySelector('.content-mh');
 const contentPers = document.querySelector('.content-pers');
-const hamburgerBtn = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.navigation-menu');
 
-// DROPDOWN MENUS 
+// dropdown menus
 
-// psyBtn.addEventListener('click', () => {
-//     if (contentPsy.style.display == '') {
-//       contentPsy.style.display = 'flex';
-//     } else {
-//       contentPsy.style.display = '';
-//     }
-// })
-// mhBtn.addEventListener('click', () => {
-//     if (contentMh.style.display == '') {
-//       contentMh.style.display = 'flex';
-//     } else {
-//       contentMh.style.display = '';
-//     }
-// })
-// persBtn.addEventListener('click', () => {
-//     if (contentPers.style.display == '') {
-//       contentPers.style.display = 'flex';
-//     } else {
-//       contentPers.style.display = '';
-//     }
-// })
-
-// HAMBURGER MENU
-// navMenu.style.display = 'none';
-hamburgerBtn.addEventListener('click', () => {
-  if (navMenu.style.display == '') {
-    navMenu.style.display = 'inline-block'; 
-  } else {
-    navMenu.style.display = '';
-  }
-
+contentPsy.addEventListener('click', () => {
+  contentPsy.classList.toggle('show');
 })
+contentMh.addEventListener('click', () => {
+  contentMh.classList.toggle('show');
+})
+contentPers.addEventListener('click', () => {
+  contentPers.classList.toggle('show');
+})
+
+
+
+
+
+
+// close the menus if user clicks outside them 
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-psy') ||!event.target.matches('.dropdown-mh') || !event.target.matches('.dropdown-pers') ) {
+    for ( let i = 0; i < contentPsy.length; i++) {
+      const openDropdown = contentPsy[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
