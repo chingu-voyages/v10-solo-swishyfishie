56dc3e904cd6dae0021187b29982f95986b19b5e
[swishyfishie] <[pclandrei@gmail.com]>
2019-07-30 03:13:45 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -1,31 +1,9 @@
-const navMenu = document.querySelector('.drp');
-const CTN = document.querySelector('.ctn-wrapper');
 const heroCont = document.querySelector('.hero-content');
-const drpBtns = document.querySelectorAll('.drpBtn')
 const modal = document.querySelector('#myModal');
 const modalBtn = document.querySelector('#modalBtn');
 const closeModal = document.querySelector('.close-btn');
 
-// dropdown menus
 
-
-drpBtns.forEach((tab, index) => {
-  tab.addEventListener('click', () =>{
-    const contentList = document.querySelectorAll('.content-list');
-      contentList.forEach((content, index) =>{
-        content.classList.remove('show');
-      })
-      contentList[index].classList.toggle('show');
-  })
-})
-
-CTN.addEventListener('click', ()=>{
-  for (let i = 0; i < navMenu.length; i++){
-    navMenu[i].children[1].classList.remove('show');
-  }
-})
-
-CTN
 
 
 // HERO ONLOAD 
