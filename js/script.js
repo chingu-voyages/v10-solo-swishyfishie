ab34d2910adb1123ec802cbc94a87bd5ad5cf1b6
[swishyfishie] <[pclandrei@gmail.com]>
2019-08-05 23:17:12 +0100

    final commits

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
-const heroCont = document.querySelector('.hero-content');
-const modal = document.querySelector('#myModal');
-const modalBtn = document.querySelector('#modalBtn');
-const closeModal = document.querySelector('.close-btn');
+import(
+  "https://cferdinandi.github.io/smooth-scroll/dist/smooth-scroll.js"
+).then(function() {
+  new SmoothScroll('a[href*="#"]'); // eslint-disable-line
+  console.log("resolved!");
+});
 
+const heroCont = document.querySelector(".hero-content");
+const modal = document.querySelector("#myModal");
+const modalBtn = document.querySelector("#modalBtn");
+const closeModal = document.querySelector(".close-btn");
 
-
-
-// HERO ONLOAD 
+// HERO ONLOAD
 
 window.onload = () => {
-  setTimeout(()=>{
-    heroCont.classList.add('hero-cont-block');
+  setTimeout(() => {
+    heroCont.classList.add("hero-cont-block");
   }, 1000);
-}
+};
 
+// Modal
 
-// Modal 
+modalBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
 
-modalBtn.addEventListener('click', ()=>{
-  modal.style.display = 'block';
-})
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-closeModal.addEventListener('click', ()=>{
-  modal.style.display = 'none';
-})
-
-window.addEventListener('click', (e)=>{
+window.addEventListener("click", e => {
   if (e.target == modal) {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   }
-})
+});
